88f963cc319542c88711fb3a1ec82e65b1628aec
Mission Marcus <missionmarcus@gmail.com>
2021-08-07 13:11:42 -0500

    added line component

--- src/components/line/line.jsx
+++ src/components/line/line.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './line.scss'
 
-const Line = () => {
+const Line = (props) => {
 
+    const {color, height} = props
+
+    return (
+        <div className='line' style={{color: color, height: height}}></div>
+    )
 }
+
+export default Line
