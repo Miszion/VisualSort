7c680dbf592eba3d4f42fe1e5dc76205f77a40dd
Mission Marcus <missionmarcus@gmail.com>
2021-08-07 13:49:53 -0500

    created randomization function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,15 +1,42 @@
 import Line from '../classes/Line'
+
+
+const generateRandom = (range) => {
+    let min = Math.ceil(1)
+    let max = Math.floor(range)
+
+    return Math.floor(Math.random() * (max- min + 1) + min)
+
+}
+
 export const generateLines = (range, startColor) => {
 
     const array = []
+    const lineArray = []
+    const randomDict = {}
 
-    for (var i = 1; i < range; i++) {
+    while (array.length < range) {
+        
+        let random = generateRandom(50)
 
-        const newLine = new Line(startColor, (10 * i));
+        while (randomDict[random]) {
+            random = generateRandom(50)
+        }
 
-        array.push(newLine)
+        randomDict[random] = 1
+
+        array.push(random)
+
     }
 
-    return array
+
+    for (var i = 0; i < array.length; i++) {
+
+        const newLine = new Line(startColor, (10 * array[i]));
+
+        lineArray.push(newLine)
+    }
+
+    return lineArray
 
 }
